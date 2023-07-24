ac9f292b0928651715c1eca4ee20e4428c80f525
redachahdi <reda.elkhattabi01@gmail.com>
2023-07-24 06:00:06 -0700

    task 001

--- sort.h
+++ sort.h
@@ -17,4 +17,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 #endif
