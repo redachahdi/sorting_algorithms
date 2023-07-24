66659456a3f1d53dc19d8b64b87cde636a496651
redachahdi <reda.elkhattabi01@gmail.com>
2023-07-24 06:21:08 -0700

    task 00003

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,19 +14,19 @@ void selection_sort(int *array, size_t size)
 	size_t k, r_step, idx;
 	int tmp;
 
-	for (step = 0; step < size - 1; step++)
+	for (r_step = 0; r_step < size - 1; r_step++)
 	{
-		idx = step;
-		for (k = step + 1; k < size; k++)
+		idx = r_step;
+		for (k = r_step + 1; k < size; k++)
 		{
 			if (array[k] < array[idx])
 				idx = k;
 		}
 
-		if (idx != step)
+		if (idx != r_step)
 		{
-			tmp = array[step];
-			array[step] = array[idx];
+			tmp = array[r_step];
+			array[r_step] = array[idx];
 			array[idx] = tmp;
 			print_array(array, size);
 		}
